7d768cd27d684816d6d1904aea2a58ed3592fff1
Tony Eneh <30419946+tony-eneh@users.noreply.github.com>
2024-01-19 22:24:13 +0100

    feat: make the soh/soc charts always go down

--- model.py
+++ model.py
@@ -1,18 +1,26 @@
 from datetime import datetime
-from random import randrange
+from random import uniform
 
 
 def getPredictions(batteryId, minChargeCyle, maxChargeCycle, step=1):
     predictions = []
+    soc = uniform(95, 100)
+    soh = uniform(95, 100)
 
     for cycle in range(int(minChargeCyle), int(maxChargeCycle), int(step)):
         prediction = {
-            "soc": randrange(1, 100),
-            "soh": randrange(1, 100),
+            "soc": soc,
+            "soh": soh,
             "createdAt": datetime.now().isoformat(),
             "batteryId": batteryId,
             "chargeCycles": cycle,
         }
+        step = int(step)
+        new_soc = soc - uniform((step), (step) * 0.5)
+        new_soh = soh - uniform((step), (step) * 0.5)
+
+        soc = new_soc if new_soc > 0 else soc
+        soh = new_soh if new_soh > 0 else soh
 
         predictions.append(prediction)
 
